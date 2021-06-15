e935d3b499bea60ce73ee18b800e815f87824191
Alexander Ward <alexander.ward1@gmail.com>
2021-06-15 13:08:07 -0500

    Offline persistence

--- images/govready-q/docker_exec_write_environment.sh
+++ images/govready-q/docker_exec_write_environment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "[ + ] Setting up environment"
 mkdir -p local
 cat << EOF > local/environment.json
 {
@@ -12,7 +13,7 @@ cat << EOF > local/environment.json
 	"db": $(echo ${DATABASE_CONNECTION_STRING-} | jq -R .),
 	"allowed_hosts": ["app", "${HOST_ADDRESS}"],
 	"govready-url": "$(echo "https://${HOST_ADDRESS}:${HOST_PORT_HTTPS}" )",
-	"okta": ${OKTA-}
+	"okta": ${OKTA-\{\}}
 }
 EOF
 
